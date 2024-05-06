7a31f4f93b79f8e75e215f0a06e58984bbe17c40
santvaz <stawfikvazquez@gmail.com>
2024-05-06 14:44:16 +0200

    :art: inserted extended themes + started dashboard template prototype

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'grape': {
+					'50': '#f8f5fd',
+					'100': '#f0edfa',
+					'200': '#e4def6',
+					'300': '#d1c3ef',
+					'400': '#b9a0e5',
+					'500': '#a079d9',
+					'600': '#905ccb',
+					'700': '#7f4ab7',
+					'800': '#6b3d9a',
+					'900': '#59347e',
+					'950': '#3d235c',
+				},
+
+			},
+		},
 	},
 	plugins: [],
 }
