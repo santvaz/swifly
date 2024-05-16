14a6649586348c4ef92e4c17aeaa41d632e894dd
santvaz <stawfikvazquez@gmail.com>
2024-05-16 11:04:17 +0200

    auth-astro configuration + .env file + Google OAuth Sign In

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import db from "@astrojs/db";
 
+import auth from "auth-astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), react()]
+  integrations: [tailwind(), db(), react(), auth()]
 });
