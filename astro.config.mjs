ba6cec486e12eb9a2215f168f1dfdda4dfc11141
santvaz <stawfikvazquez@gmail.com>
2024-05-16 14:31:17 +0200

    SSR Adapter (Vercel) + Google OAuth

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import db from "@astrojs/db";
-
 import auth from "auth-astro";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), react(), auth()]
+  integrations: [tailwind(), db(), react(), auth()],
+  output: "server",
+  adapter: vercel()
 });
