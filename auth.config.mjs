8848c21afff4b523540c434ad8b5141cdaf9bd1a
santvaz <stawfikvazquez@gmail.com>
2024-05-17 15:00:18 +0200

    GitHub OAuth + Login.astro styling

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "auth-astro";
 import Google from "@auth/core/providers/google";
+import GitHub from "@auth/core/providers/github";
 
 export default defineConfig({
     providers: [
@@ -7,5 +8,9 @@ export default defineConfig({
             clientId: import.meta.env.GOOGLE_CLIENT_ID,
             clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
         }),
-    ]
+        GitHub({
+            clientId: import.meta.env.GITHUB_CLIENT_ID,
+            clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+        }),
+    ],
 })
