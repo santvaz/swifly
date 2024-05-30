f8734b978de1b73c3c13ef72a352e0da79e6c1f7
santvaz <stawfikvazquez@gmail.com>
2024-05-30 14:43:35 +0200

    api haven't fixed listing projects in html

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,18 @@ import db from "@astrojs/db";
 import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), react(),
-  auth({
-    providers: [
-    ],
+  integrations: [tailwind(), db(), react(), auth({
+    providers: [],
     db: {
       // db connection
       client: 'astrodb',
       connection: process.env.DATABASE_URL
     }
-  })],
+  }), sitemap()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
--- src/pages/api/projects/[userId].ts
+++ src/pages/api/projects/[userId].ts
@@ -1,5 +1,5 @@
 import type { APIContext } from "astro";
-import { db, Projects, Users, eq, ne } from "astro:db";
+import { db, Projects, Users, eq } from "astro:db";
 
 export const prerender = true;
 
@@ -19,15 +19,24 @@ export async function GET({ params }: APIContext) {
   const { userId } = params;
   console.log("userId", userId);
 
+  const user = await (
+    await db.select().from(Users).where(eq(Users.id, userId))
+  ).at(0);
+
   if (!userId) {
     throw new Error("User id is required");
   }
 
-  const projects = await db.select().from(Projects).where(eq(Projects.user_creator, userId));
+  const projects = await db
+    .select()
+    .from(Projects)
+    .where(eq(Projects.user_creator, userId));
 
   if (projects.length === 0) {
-    throw new Error(`Projects from user id ${userId} not found`);
+    throw new Error(
+      `Projects from user id ${userId} not found: could be because we couldn't fetch results correctly or because the user doesn't have any projects created.`
+    );
   }
-  
+
   return new Response(JSON.stringify(projects));
 }
