4a75b5538ce518c53ce3436e71ccb4efbfec78d8
santvaz <stawfikvazquez@gmail.com>
2024-06-08 12:53:41 +0200

    Trying to fix dynamic paths for deploy v2

--- src/lib/services/ProjectListService.ts
+++ src/lib/services/ProjectListService.ts
@@ -1,5 +1,7 @@
 export const getProjectListByUserId = async (userId: string) => {
-  const response = await fetch(`www.swifly.app/api/project-list/${userId}`);
+  const response = await fetch(
+    `http://localhost:4321/api/project-list/${userId}`
+  );
   const data = await response.json();
   return data;
 };
