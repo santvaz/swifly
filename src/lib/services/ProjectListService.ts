53a0060f86d1d5ef33d456859eda05d6c9acbdc0
santvaz <stawfikvazquez@gmail.com>
2024-06-08 20:09:53 +0200

    v6666

--- src/lib/services/ProjectListService.ts
+++ src/lib/services/ProjectListService.ts
@@ -1,40 +1,13 @@
-// export const getProjectListByUserId = async (userId: string) => {
-//   const response = await fetch(
-//     `https://swifly-app-git-deploy-santvazs-projects.vercel.app/api/project-list/${userId}`
-//   );
-//   const data = await response.json();
-//   return data;
-// };
-
-// // ProjectListService.ts
-// export const getProjectListByUserId = async (userId: string) => {
-//   const response = await fetch(
-//     `https://swifly-app-git-deploy-santvazs-projects.vercel.app/api/project-list/${userId}`
-//   );
-//   if (!response.ok) {
-//     throw new Error(`Error fetching project list: ${response.statusText}`);
-//   }
-//   const data = await response.json();
-//   return data;
-// };
-export const getProjectListByUserId = async (
-  userId: string,
-  userToken: string
-) => {
+export const getProjectListByUserId = async (userId: string) => {
   const response = await fetch(
-    `https://swifly-app-git-deploy-santvazs-projects.vercel.app/api/project-list/${userId}`,
-    {
-      headers: {
-        Authorization: `Bearer ${userToken}`,
-      },
-    }
+    `http://localhost:4321/api/project-list/${userId}`
   );
-  if (response.status === 401) {
-    throw new Error("Unauthorized: Invalid or expired token");
-  }
+
   if (!response.ok) {
     throw new Error(`Error fetching project list: ${response.statusText}`);
   }
+
   const data = await response.json();
+  console.log(data);
   return data;
 };
